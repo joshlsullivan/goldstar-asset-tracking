b5052470e38491970634f75a1868cf87c48c4e1f
Josh Sullivan <josh@magnolia.technology>
2018-02-07 14:32:50 -0600

    updated static

--- sm8_asset_tracking/settings.py
+++ sm8_asset_tracking/settings.py
@@ -134,7 +134,7 @@ DATABASES['default'].update(db_from_env)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
