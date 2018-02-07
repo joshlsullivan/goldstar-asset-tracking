6c8d0180f5168eec443c7cd44d919c6cc951ab77
Josh Sullivan <josh@magnolia.technology>
2018-02-07 13:10:16 -0600

    updated settings static

--- sm8_asset_tracking/settings.py
+++ sm8_asset_tracking/settings.py
@@ -124,6 +124,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
