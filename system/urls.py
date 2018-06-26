4e48a4f2c32e11d195de4f141bf0dc1aa7aea059
Josh Sullivan <josh@magnolia.technology>
2018-06-26 09:43:59 -0400

    updated query

--- system/urls.py
+++ system/urls.py
@@ -5,4 +5,5 @@ from system.views import SystemListView
 app_name = 'system'
 urlpatterns = [
     path('', SystemListView.as_view(), name='system-list'),
+    path('system/<monitoring_type>/', MonitoringTypeListView.as_view(), name='monitoring_type'),
 ]
--- system/views.py
+++ system/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from client.models import System
@@ -6,3 +7,10 @@ class SystemListView(LoginRequiredMixin, ListView):
     model = System
     fields = '__all__'
     template_name = 'system/system_list.html'
+
+class MonitoringTypeListView(LoginRequiredMixin, ListView):
+    template_name = 'system/monitoring_type_list.html'
+
+    def get_queryset(self):
+        self.monitoring_type = get_object_or_404(System, name=self.kwargs['monitoring_type'])
+        return System.objects.filter(monitoring_type=self.monitoring_type)
