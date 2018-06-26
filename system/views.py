6e00f6e26757f5add90513e83fcfa137815d9007
Josh Sullivan <josh@magnolia.technology>
2018-06-26 09:54:14 -0400

    udpated queryset

--- system/views.py
+++ system/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_list_or_404
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from client.models import System
@@ -12,5 +12,5 @@ class MonitoringTypeListView(LoginRequiredMixin, ListView):
     template_name = 'system/monitoring_type_list.html'
 
     def get_queryset(self):
-        self.monitoring_type = get_object_or_404(System, monitoring_type=self.kwargs['monitoring_type'])
+        self.monitoring_type = get_list_or_404(System, monitoring_type=self.kwargs['monitoring_type'])
         return System.objects.filter(monitoring_type=self.monitoring_type)
