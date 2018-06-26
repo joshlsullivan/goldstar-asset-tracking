f1e605116f3991467c7bbb8039d581fc8d17330a
Josh Sullivan <josh@magnolia.technology>
2018-06-26 09:48:43 -0400

    updated queryset

--- system/views.py
+++ system/views.py
@@ -12,5 +12,5 @@ class MonitoringTypeListView(LoginRequiredMixin, ListView):
     template_name = 'system/monitoring_type_list.html'
 
     def get_queryset(self):
-        self.monitoring_type = get_object_or_404(System, name=self.kwargs['monitoring_type'])
+        self.monitoring_type = get_object_or_404(System, monitoring_type=self.kwargs['monitoring_type'])
         return System.objects.filter(monitoring_type=self.monitoring_type)
