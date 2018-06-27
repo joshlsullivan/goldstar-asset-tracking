655410702cf4ebbae18e1752e2f1fdaaa0447d52
Josh Sullivan <josh@magnolia.technology>
2018-06-27 16:25:18 -0400

    added import

--- system/views.py
+++ system/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
