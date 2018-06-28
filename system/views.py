914c72463618659f098ad4991781114bc7a363a9
Josh Sullivan <josh@magnolia.technology>
2018-06-28 00:14:31 -0400

    updated hit rate

--- system/views.py
+++ system/views.py
@@ -63,6 +63,19 @@ class SystemsKPIReport(View):
         oct_cur_non_con = System.objects.filter(contract_date__year=current_year).filter(contract_date__month='10').filter(contracted='N').count()
         nov_cur_non_con = System.objects.filter(contract_date__year=current_year).filter(contract_date__month='11').filter(contracted='N').count()
         dec_cur_non_con = System.objects.filter(contract_date__year=current_year).filter(contract_date__month='12').filter(contracted='N').count()
+        jan_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='01').count()
+        jan_cur_rate = (jan_cur_man / jan_cur_jobs * 100) if total_jobs != 0 else 0
+        feb_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='02').count()
+        mar_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='03').count()
+        apr_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='04').count()
+        may_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='05').count()
+        jun_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='06').count()
+        jul_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='07').count()
+        aug_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='08').count()
+        sep_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='09').count()
+        oct_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='10').count()
+        nov_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='11').count()
+        dec_cur_jobs = Job.objects.filter(job_date__year=current_year).filter(job_date__month='12').count()
         jan_cur_man = Job.objects.filter(job_date__year=current_year).filter(job_date__month='01').filter(job_category='Maintenance').count()
         feb_cur_man = Job.objects.filter(job_date__year=current_year).filter(job_date__month='02').filter(job_category='Maintenance').count()
         mar_cur_man = Job.objects.filter(job_date__year=current_year).filter(job_date__month='03').filter(job_category='Maintenance').count()
@@ -118,6 +131,7 @@ class SystemsKPIReport(View):
             {
                 'current_year':current_year,
                 'previous_year':previous_year,
+                'jan_cur_rate':jan_cur_rate,
                 'jan_cur_con':jan_cur_con,
                 'feb_cur_con':feb_cur_con,
                 'mar_cur_con':mar_cur_con,
