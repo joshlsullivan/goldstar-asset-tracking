dcd8ecd6778040b5db0f07518cbc0d26476d85f9
Josh Sullivan <josh@magnolia.technology>
2018-06-27 22:48:59 -0400

    updated

--- sm8event/views.py
+++ sm8event/views.py
@@ -127,16 +127,11 @@ def update_categories():
     auth = ('josh+goldsmith@misllc.com', '9793')
     jobs = requests.get(url, auth=auth).json()
     for job in jobs:
-        try:
-            category = requests.get('https://api.servicem8.com/api_1.0/category/{}.json'.format(job['category_uuid']), auth=auth).json()
-            old_job = Job.objects.get(job_uuid=job['uuid'])
-            print(old_job)
-            old_job.job_category = category['name']
-            old_job.save()
-            return True
-        except ValueError:
-            print("Error occured")
-            pass
+        category = requests.get('https://api.servicem8.com/api_1.0/category/{}.json'.format(job['category_uuid']), auth=auth).json()
+        old_job = Job.objects.get(job_uuid=job['uuid'])
+        print(old_job)
+        old_job.job_category = category['name']
+        old_job.save()
     return job
 
 
