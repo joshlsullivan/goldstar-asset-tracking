6e75a4f4c83aeb6905b23b6c83a127c15eea065f
Josh Sullivan <josh@magnolia.technology>
2018-07-16 10:54:06 -0400

    updated admin

--- client/admin.py
+++ client/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Client, Job, Task, System
 
-@admin.register(Client, Job, Task, System)
-class ClientAdmin(admin.ModelAdmin):
-    pass
+class ClientAdmin(admin.ModelsAdmin):
+    list_display = ('name')
+    search_fields = ['name']
+
+class SystemAdmin(admin.ModelsAdmin):
+    list_display = ['client', 'get_system_type_display']
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(System, SystemAdmin)
